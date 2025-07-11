bb44beea446a2f7a1d8c1c209d001f202dc02cf2
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2025-07-11 12:56:27 +0700

    forcing individual script to use sudo; installing dependencies

--- init.sh
+++ init.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-./hyprutils/main.sh "$@"
+sudo apt install dh_make build-essential git make gawk flex bison libgmp-dev libmpfr-dev libmpc-dev python3 binutils perl libisl-dev libzstd-dev tar gzip bzip2
+sudo ./hyprutils/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./hyprwayland-scanner/main.sh "$@"
+sudo ./hyprwayland-scanner/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./hyprlang/main.sh "$@"
+sudo ./hyprlang/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./hyprgraphics/main.sh "$@"
+sudo ./hyprgraphics/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./aquamarine/main.sh "$@"
+sudo ./aquamarine/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./hyprcursor/main.sh "$@"
+sudo ./hyprcursor/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./xdg-desktop-portal-hyprland/main.sh "$@"
+sudo ./xdg-desktop-portal-hyprland/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
-./hyprland-git/main.sh "$@"
+sudo ./hyprland-git/main.sh "$@"
 sudo dpkg -i *.deb
 rm *.deb
