4eea2a5ff53c25800317078920f0ae0d8e446f9b
imchocomint <mintgaming@outlook.com.vn>
2025-08-11 21:49:54 +0700

    This is a big update
    - Fixing a bug in hyprland-qtsupport build process which do not move the package outside
    - Adding support for Hyprland tagged release
    - Obsoleting hyprland-git package

--- build/hyprland/main.sh
+++ build/hyprland/main.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+VERSION="0.50.1"
+
 function check_architecture() {
     local arch=$(uname -m)
     if [[ "$arch" == "x86_64" ]]; then
@@ -15,7 +17,7 @@ function check_architecture() {
     export COMPUTER_ARCH
 }
 check_architecture
-cd ./hyprland-qtsupport
+cd ./hyprland
 if [[ "$COMPUTER_ARCH" == "amd64" ]]; then
     cp ./build-config/amd64-v3.sh ./build-config.sh
 elif [[ "$COMPUTER_ARCH" == "x86" ]]; then
@@ -27,22 +29,20 @@ source ./build-config.sh
 
 echo "$PIKA_BUILD_ARCH" > pika-build-arch
 
-VERSION="0.1.0"
-
 # Clone Upstream
-git clone --recurse-submodules https://github.com/hyprwm/hyprland-qt-support
-cp -rvf ./debian ./hyprland-qt-support/
-cd ./hyprland-qt-support/
+git clone --recurse-submodules https://github.com/hyprwm/hyprland -b v$VERSION
+cp -rvf ./debian ./hyprland/
+cd ./hyprland
 
 # Get build deps
-LOGNAME=root dh_make -n -y -l -p hyprland-qt-support_"$VERSION" || echo "dh-make: Ignoring Last Error"
 apt-get build-dep ./ -y
 
 # Build package
+LOGNAME=root dh_make -n -y -l -p hyprland_latest || echo "dh-make: Ignoring Last Error"
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
-sudo rm -rf hyprland-qt-support
+sudo rm -rf hyprland
 cd ../
-mv ./hyprland-qtsupport/*.deb ../
+mv ./hyprland/*.deb ./
