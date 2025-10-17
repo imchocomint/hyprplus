c2a124999819431fd03cda64573ba415bfb2795b
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2025-10-17 21:38:14 +0700

    Attempting to fix the script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,86 +4,22 @@
 
 BASE_URL="https://github.com/imchocomint/hyprplus/releases/latest/download/"
 
-
-# if download is slow, uncomment the next line (and thus comment the one above)
-# BASE_URL="https://sourceforge.net/projects/hyprplus-mirror/files/1.0-0.51.1-hf2/"
+# BASE_URL="https://sourceforge.net/projects/hyprplus-mirror/files/<release tag>/"
 
 # Define all packages to download and install.
 all_packages=(
-    "aquamarine-dbgsym-latest.deb"
-    "aquamarine-latest.deb"
-    "hyprcursor-util-dbgsym-latest.deb"
-    "hyprcursor-util-latest.deb"
     "hyprland-dbgsym-latest.deb"
     "hyprland-latest.deb"
     "hyprland-qt-support-dbgsym-latest.deb"
     "hyprland-qt-support-latest.deb"    
     "hyprland-qtutils-dbgsym-latest.deb"
     "hyprland-qtutils-latest.deb"
-    "hyprwayland-scanner-dbgsym-latest.deb"
-    "hyprwayland-scanner-latest.deb"
-    "libaquamarine-dev-latest.deb"
-    "libhyprcursor-dbgsym-latest.deb"
-    "libhyprcursor-dev-latest.deb"
-    "libhyprcursor-latest.deb"
-    "libhyprgraphics-dbgsym-latest.deb"
-    "libhyprgraphics-dev-latest.deb"
-    "libhyprgraphics-latest.deb"
-    "libhyprlang-dbgsym-latest.deb"
-    "libhyprlang-dev-latest.deb"
-    "libhyprlang-latest.deb"
-    "libhyprutils-dbgsym-latest.deb"
-    "libhyprutils-dev-latest.deb"
-    "libhyprutils-latest.deb"
-    "libhyprwayland-scanner-latest.deb"
-    "xdg-desktop-portal-hyprland-dbgsym-latest.deb"
-    "xdg-desktop-portal-hyprland-dev-latest.deb"
-    "xdg-desktop-portal-hyprland-latest.deb"
 )
 
-# --- Define package groups based on your requested order ---
-
-hyprutils_packages=(
-    "libhyprutils-latest.deb"
-    "libhyprutils-dev-latest.deb"
-    "libhyprutils-dbgsym-latest.deb"
-)
 
 hyprland_packages=(
     "hyprland-latest.deb"
     "hyprland-dbgsym-latest.deb"
-)
-
-hyprcursor_packages=(
-    "hyprcursor-util-latest.deb"
-    "hyprcursor-util-dbgsym-latest.deb"
-    "libhyprcursor-latest.deb"
-    "libhyprcursor-dev-latest.deb"
-    "libhyprcursor-dbgsym-latest.deb"
-)
-
-hyprlang_packages=(
-    "libhyprlang-latest.deb"
-    "libhyprlang-dev-latest.deb"
-    "libhyprlang-dbgsym-latest.deb"
-)
-
-hyprgraphics_packages=(
-    "libhyprgraphics-latest.deb"
-    "libhyprgraphics-dev-latest.deb"
-    "libhyprgraphics-dbgsym-latest.deb"
-)
-
-aquamarine_packages=(
-    "aquamarine-latest.deb"
-    "libaquamarine-dev-latest.deb"
-    "aquamarine-dbgsym-latest.deb"
-)
-
-hyprwayland_scanner_packages=(
-    "hyprwayland-scanner-latest.deb"
-    "hyprwayland-scanner-dbgsym-latest.deb"
-    "libhyprwayland-scanner-latest.deb"
 )
 
 hyprland_qtutils_packages=(
@@ -96,22 +32,11 @@ hyprland_qtsupport_packages=(
     "hyprland-qt-support-dbgsym-latest.deb"
 )
 
-xdg_desktop_packages=(
-    "xdg-desktop-portal-hyprland-latest.deb"
-    "xdg-desktop-portal-hyprland-dev-latest.deb"
-    "xdg-desktop-portal-hyprland-dbgsym-latest.deb"
-)
-
-# --- Download all packages ---
-
-# Define a directory for downloads.
-DOWNLOAD_DIR="hypr_pkgs"
+DOWNLOAD_DIR="hyprplus"
 mkdir -p "$DOWNLOAD_DIR"
 cd "$DOWNLOAD_DIR" || { echo "Failed to enter download directory."; exit 1; }
 echo "--- Installing dependencies ---"
 sudo apt install libliftoff0 libqt6core6t64 libqt6gui6 libqt6qml6 libqt6quickcontrols2-6 libqt6widgets6* qml6-module-qtquick-* libxcb* libudis86* libdisplay-info2 libzip5 libzip-dev libtomlplusplus* libmagic1t64 libmagic-dev libmagic-mgc libgles2 libre2-dev libre2-11 
-sudo ln -s /usr/lib/x86_64-linux-gnu/libdisplay-info.so.2 /usr/lib/x86_64-linux-gnu/libdisplay-info.so.1
-sudo ldconfig
 echo "--- Starting download of all Hypr* files ---"
 
 for filename in "${all_packages[@]}"; do
@@ -154,31 +79,10 @@ install_and_fix() {
     echo "Dependency fix attempt complete for ${group_name}."
 }
 
-install_and_fix hyprutils_packages "libhyprutils"
-install_and_fix hyprlang_packages "libhyprlang"
-install_and_fix hyprgraphics_packages "libhyprgraphics"
-install_and_fix hyprcursor_packages "libhyprcursor"
-install_and_fix aquamarine_packages "aquamarine"
-install_and_fix hyprwayland_scanner_packages "hyprwayland-scanner"
+sudo apt install libhyprcursor libhyprutils libhyprlang libhyprgraphics hyprland-protocols hyprwayland-scanner xdg-desktop-portal-hyprland libaquamarine
 install_and_fix hyprland_qtsupport_packages "hyprland-qt-support"
 install_and_fix hyprland_qtutils_packages "hyprland-qtutils"
 install_and_fix hyprland_packages "hyprland"
-install_and_fix xdg_desktop_packages "xdg-desktop-portal-hyprland"
-echo "---- Installation part 1 complete ----"
-sleep 3s
-
-echo "If you see errors related to dpkg while installing packages above, please ignore them"
-echo "The next few seconds will be used to fix them"
-echo "This message will be retained on screen for 10 seconds so you could read and copy the errors if you feel that it is needed"
-
-sleep 10s
-
-echo "Begin installation part 2"
-cd hypr_pkgs
-sudo dpkg -i --force-overwrite *.deb
-sleep 3s
-echo "Installation part 2 complete"
-
 
 echo ""
 echo "--- Installation process complete. ---"
