abfcbe0c3736b14e8b729a16fc25ef21f51e1f6e
imchocomint <mintgaming@outlook.com.vn>
2025-11-08 16:02:36 +0700

    Retiring hyprland-guiutils and bring back qtutils

--- build/init.sh
+++ build/init.sh
@@ -2,6 +2,6 @@
 sudo apt update > /dev/null 2>&1
 sudo apt install dh-make build-essential git make python3 binutils perl tar gzip bzip2 libgbm-dev libre2-dev libxcb-icccm4-dev libxcb-res0-dev libxcb-errors-dev libtomlplusplus-dev qt6-wayland rename
 sudo ./hyprland-qtsupport/main.sh "$@"
-sudo ./hyprland-guiutils/main.sh "$@"
+sudo ./hyprland-qtutils/main.sh "$@"
 sudo ./hyprland/main.sh "$@"
 bash rename.sh
