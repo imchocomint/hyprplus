105ab0c5dacf91a21b2da67774a31f14d5dad389
imchocomint <mintgaming@outlook.com.vn>
2025-12-04 19:32:11 +0700

    Fix rename

--- build/rename.sh
+++ build/rename.sh
@@ -1,2 +1,2 @@
-rename 's/^(hypr(land|land-guiutils|land-qt-support)-dbgsym)(.*)\.deb$/$1-latest.deb/' *.deb
-rename 's/^(hypr(land|land-guiutils|land-qt-support))(.*)\.deb$/$1-latest.deb/' *.deb
+rename 's/^(hypr(land|land-qtutils|land-qt-support)-dbgsym)(.*)\.deb$/$1-latest.deb/' *.deb
+rename 's/^(hypr(land|land-qtutils|land-qt-support))(.*)\.deb$/$1-latest.deb/' *.deb
