9da88130460cfbc1f2ee1af127843217829a141b
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2026-01-02 14:02:52 +0700

    Update init.sh

--- build/init.sh
+++ build/init.sh
@@ -3,4 +3,5 @@ sudo apt update > /dev/null 2>&1
 sudo apt install dh-make build-essential git make python3 binutils perl tar gzip bzip2 libgbm-dev libre2-dev libxcb-icccm4-dev libxcb-res0-dev libxcb-errors-dev libtomlplusplus-dev qt6-wayland rename
 sudo ./hyprland-qtsupport/main.sh "$@"
 sudo ./hyprland-qtutils/main.sh "$@"
+sudo ./hyprwire/main.sh "$@"
 sudo ./hyprland/main.sh "$@"
