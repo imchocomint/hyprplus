56c85fe338befd925d5fe9e9477837758e6e59a2
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2026-09-06 19:49:13 +0700

    Update build-actions.sh

--- build/build-actions.sh
+++ build/build-actions.sh
@@ -1,5 +1,5 @@
 apt update > /dev/null 2>&1
-apt install dh-make build-essential git make python3 binutils perl tar gzip bzip2 libgbm-dev libre2-dev libxcb-icccm4-dev libxcb-res0-dev libxcb-errors-dev libtomlplusplus-dev qt6-wayland
+apt install dh-make build-essential git make python3 binutils perl tar gzip bzip2 libgbm-dev libre2-dev libxcb-icccm4-dev libxcb-res0-dev libxcb-errors-dev libtomlplusplus-dev qt6-wayland libffi-dev
 
 # Build hws
 ./hyprwayland-scanner/main.sh "$@"
